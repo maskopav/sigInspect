5fea5b0f6749d7ec40be3a305b85f62fed8225c1
maskopav <maskopav@fel.cvut.cz>
2025-04-14 21:40:46 +0200

    changed weights for feature selection

--- runFeatureSelection.m
+++ runFeatureSelection.m
@@ -167,7 +167,8 @@ for artifactIdx=2:4
     cleanToArtifactRatios = [1.5, 2, 2.5, 3, 3.5, 4];
     for cleanToArtifactIdx= 1:length(cleanToArtifactRatios)
         cleanToArtifactRatio = cleanToArtifactRatios(cleanToArtifactIdx);
-        costWeight = computeClassWeights(Y_fs_train);
+        alpha = 0.5;
+        costWeight = computeClassWeights(Y_fs_train, alpha);
         costMatrix = [0 1; costWeight 0];
     
         for idx = 1:length(criteriaList)
